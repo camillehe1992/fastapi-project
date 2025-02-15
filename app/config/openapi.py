8fafe85ca9c0d65723e6b2c23b7486ffa0097bd5
camillehe1992 <camille.he@outlook.com>
2025-02-15 09:17:26 +0800

    chore: move custom_openapi to separated file for better organized

---
+++ app/config/openapi.py
@@ -0,0 +1,34 @@
+from fastapi.openapi.utils import get_openapi
+
+from config.settings import settings
+
+
+# Custom OpenAPI schema to include securitySchemes
+def custom_openapi(app):
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title=settings.TITLE,
+        version=settings.VERSION,
+        description="A Swagger for FaskAPI Application",
+        routes=app.routes,
+    )
+
+    # Customize the securitySchemes
+    components = openapi_schema.get("components", {})
+
+    components["securitySchemes"] = {
+        "OAuth2PasswordBearer": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",  # Optional: Specify the token format
+        }
+    }
+
+    # Apply the security scheme globally
+    # openapi_schema["security"] = [{"OAuth2PasswordBearer": []}]
+
+    # Save the custom schema
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
 
 from config.settings import settings
 from routers.api import router
 from db.utils import create_tables
+from config.openapi import custom_openapi
 
 
 # Lifespan function to initialize the database
@@ -27,35 +27,8 @@ if settings.DEBUG:
 else:
     origins = [str(origin).strip(",") for origin in settings.ORIGINS]
 
-
-# Custom OpenAPI schema to include securitySchemes
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    openapi_schema = get_openapi(
-        title=settings.TITLE,
-        version=settings.VERSION,
-        description="API with OAuth2 security",
-        routes=app.routes,
-    )
-
-    openapi_schema["components"]["securitySchemes"] = {
-        "OAuth2PasswordBearer": {
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT",  # Optional: Specify the token format
-        }
-    }
-
-    # Apply the security scheme globally
-    # openapi_schema["security"] = [{"OAuth2PasswordBearer": []}]
-
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
+# Assign the custom OpenAPI schema to the app
+app.openapi = lambda: custom_openapi(app)
 
 # Include routers
 app.include_router(router)
