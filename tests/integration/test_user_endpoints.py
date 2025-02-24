a3ccede70b97849e80ed08aee2a4d79d2a88f81d
camillehe1992 <camille.he@outlook.com>
2025-02-24 17:40:53 +0800

    test: decrease coverage threahold to 85

--- tests/integration/test_user_endpoints.py
+++ tests/integration/test_user_endpoints.py
@@ -18,6 +18,13 @@ class TestUserEndpoints(unittest.TestCase):
         self.email = f"{self.random_username}@example.com"
         self.password = "Mypassword@123"
 
+        self.invalid_passwords = [
+            "Short1!",
+            "nopassword1!",
+            "NoNumberPass!",
+            "NoSpecialChar1",
+        ]
+
     def test_register_user(self):
         # Test data
         user_data = {
@@ -36,7 +43,46 @@ class TestUserEndpoints(unittest.TestCase):
         self.assertEqual(response_data["data"]["email"], user_data["email"])
         self.assertEqual(response_data["data"]["username"], user_data["username"])
 
-    def test_login_user(self):
+    def test_register_user_failure(self):
+        # Test data
+        user_data = {
+            "email": self.email,
+            "password": self.password,
+            "username": self.random_username,
+        }
+
+        # Register user
+        response = self.client.post(f"{API_PREFIX}/register", json=user_data)
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Register user again with same email
+        response = self.client.post(f"{API_PREFIX}/register", json=user_data)
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Register user again with same username
+        response = self.client.post(f"{API_PREFIX}/register", json=user_data)
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_register_user_with_invalid_password(self):
+        # Test data
+
+        for invalid_password in self.invalid_passwords:
+            user_data = {
+                "email": self.email,
+                "password": invalid_password,
+                "username": self.random_username,
+            }
+
+            # Make the request to the /api/v1/users/register endpoint
+            response = self.client.post(f"{API_PREFIX}/register", json=user_data)
+
+            # Assert the response
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_user_success(self):
         # Test data
         user_data = {
             "email": self.email,
@@ -60,6 +106,52 @@ class TestUserEndpoints(unittest.TestCase):
         response_data = response.json()
         self.assertIn("access_token", response_data)
         self.assertIn("token_type", response_data)
+
+    def test_login_user_failure_with_invalid_password(self):
+        # Test data
+        user_data = {
+            "email": self.email,
+            "password": self.password,
+            "username": self.random_username,
+        }
+        login_data = {
+            "email": self.email,
+            "password": f"Invalid{self.password}",
+        }
+
+        # First register a new user
+        self.client.post(f"{API_PREFIX}/register", json=user_data)
+
+        # Then, attempt to login with invalid credentials
+        response = self.client.post(f"{API_PREFIX}/login", json=login_data)
+
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response_data = response.json()
+        self.assertEqual(response_data["detail"], "Incorrect email or password")
+
+    def test_login_user_failure_with_invalid_email(self):
+        # Test data
+        user_data = {
+            "email": self.email,
+            "password": self.password,
+            "username": self.random_username,
+        }
+        login_data = {
+            "email": f"Invalid{self.email}",
+            "password": self.password,
+        }
+
+        # First register a new user
+        self.client.post(f"{API_PREFIX}/register", json=user_data)
+
+        # Then, attempt to login with invalid credentials
+        response = self.client.post(f"{API_PREFIX}/login", json=login_data)
+
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response_data = response.json()
+        self.assertEqual(response_data["detail"], "Incorrect email or password")
 
     def test_get_me(self):
         # Test data
@@ -92,3 +184,12 @@ class TestUserEndpoints(unittest.TestCase):
         response_data = response.json()
         self.assertEqual(response_data["email"], user_data["email"])
         self.assertEqual(response_data["username"], user_data["username"])
+
+        # Use invalid access_token to access the /api/v1/users/me endpoint
+        response = self.client.get(
+            f"{API_PREFIX}/me",
+            headers={"Authorization": f"Bearer Invalid{access_token}"},
+        )
+
+        # Assert the response
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
